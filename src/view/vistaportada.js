a1cbe63014ac503cba8bf8d7c8d476911f33ec90
Antonio Manuel Figueroa Pinilla <tonifp1729@gmail.com>
2024-03-01 12:30:42 +0100

    Modificando CSS. Completando funcionamiento de la vistaPortada

---
+++ src/view/vistaportada.js
@@ -0,0 +1,4 @@
+// setTimeout(function() {
+//     document.getElementById('vistaPortada').style.display = 'none';
+//     document.getElementById('vistaInicio').style.display = 'block';
+// }, 15000);
